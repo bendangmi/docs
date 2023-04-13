ed9707ad8d8729bd3555e0f12649fc7d7f0f093b
本当迷 <1474051104@qq.com>
2023-04-13 12:53:12 +0800

    first commit

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,17 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+export default defineUserConfig({
+  base: "/",
+  locales: {
+    "/": {
+      lang: "zh-CN",
+      title: "本当迷博客",
+      description: "一个00后编程爱好者",
+    },
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
